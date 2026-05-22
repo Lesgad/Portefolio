b83e9d630ac9a4a10990640fed0ec43ec36dbf3b
mamab <mael.gadou@gmail.com>
2026-05-22 13:44:37 +0200

    Ajout du premier élément du hero le métier/étudiant

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+export default function Hero() {
+  return (
+    <section id="accueil" className="w-full bg-white">
+      <div style={{ paddingLeft: '120px', paddingTop: '110px' }}>
+
+        <span
+          style={{
+            fontFamily: 'Nunito, sans-serif',
+            fontWeight: 700,
+            fontSize: '20px',
+            textTransform: 'uppercase',
+            color: '#FDC435',
+          }}
+        >
+          Etudiant ingénieur
+        </span>
+
+      </div>
+    </section>
+  )
+}
